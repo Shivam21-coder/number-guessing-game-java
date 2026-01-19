83e548f6041e93388e76530dc7a05763ba1f181f
shivam gehlot <shivamgehlot1632003@gmail.com>
2026-01-19 20:38:37 +0530

    Initial commit - Number Guessing Game in Java

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+        int randomNumber = random.nextInt(10) + 1; // ✅ FIXED
+
+        System.out.println("Number Guessing Game");
+        System.out.println("Guess a Number between 1-10");
+        // System.out.println("DEBUG Random Number: " + randomNumber);
+
+        do {
+            System.out.print("Enter a guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < randomNumber) {
+                System.out.println("TOO LOW! Try again");
+            }
+            else if (guess > randomNumber) {
+                System.out.println("TOO HIGH! Try again");
+            }
+            else {
+                System.out.println("CORRECT! The number was " + randomNumber);
+                System.out.println("# of attempts: " + attempts);
+            }
+
+        } while (guess != randomNumber);
+
+        scanner.close();
+    }
+}
